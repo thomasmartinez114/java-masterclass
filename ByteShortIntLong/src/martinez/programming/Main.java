1beee309b4cf7ba40c46267cb6916698e1685048
Thomas Martinez <tmartinez114@gmail.com>
2020-08-27 01:11:08 -0400

    completed Primitive Types and Hello World

---
+++ ByteShortIntLong/src/martinez/programming/Main.java
@@ -0,0 +1,16 @@
+package martinez.programming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue); // -2 billion
+        System.out.println("Integer Maximum Value = " + myMaxIntValue); // 2 billion
+        System.out.println("Busted MAX Value " + (myMaxIntValue + 1));  // Overflow - number too large which made it -2 billion
+        System.out.println("Busted MIN Value " + (myMinIntValue - 1)); // Overflow - number is too large which made it 2 billion
+    }
+}
--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,6 +1,12 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Team");
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
     }
 }
