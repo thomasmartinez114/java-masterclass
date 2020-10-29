e3921ebbbfe7e4cf7a4cac11625b3cc4fc8a3444
Thomas Martinez <tmartinez114@gmail.com>
2020-10-29 19:50:32 -0400

    Char and Booleans - set the values

---
+++ CharAndBoolean/src/martinez/company/Main.java
@@ -0,0 +1,18 @@
+package martinez.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyright = '\u00a9';
+        System.out.println(myCopyright);
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
