47aa4026570698ad9f5b40e06e31226718a820c3
Thomas Martinez <tmartinez114@gmail.com>
2020-10-29 21:26:01 -0400

    Ternary Operator

--- 03 - First Steps/Operators/src/martinez/company/Main.java
+++ 03 - First Steps/Operators/src/martinez/company/Main.java
@@ -9,5 +9,17 @@ public class Main {
             System.out.println("It is not an alien!");
             System.out.println("And I am scared of aliens.");
         }
+
+        boolean isCar = false;
+        if (isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+
+        // Tenary Operator
+        isCar = true;
+        boolean wasCar = isCar ? true : false;  
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
     }
 }
