5becc3797079b530b08e0a8025c4ef57d376db7e
Thomas Martinez <tmartinez114@gmail.com>
2020-10-29 21:58:26 -0400

    Operator Challenge

--- 03 - First Steps/Operators/src/martinez/company/Main.java
+++ 03 - First Steps/Operators/src/martinez/company/Main.java
@@ -17,9 +17,23 @@ public class Main {
 
         // Tenary Operator
         isCar = true;
-        boolean wasCar = isCar ? true : false;  
+        boolean wasCar = isCar ? true : false;
         if (wasCar) {
             System.out.println("wasCar is true");
         }
+
+        // Challenge
+        double doubleOne = 20.00d;
+        double doubleTwo = 80.00d;
+        double doubleSum = (doubleOne + doubleTwo) * 100.00d;
+        System.out.println("Total values: " + doubleSum);
+        double remainder = doubleSum % 40.00d;
+        System.out.println("Remainder: " + remainder);
+        boolean isZero = remainder == 0 ? true : false;
+        System.out.println("isZero: " + isZero);
+        if (!isZero) {
+            System.out.println("Got some remainder");
+        }
+
     }
 }
