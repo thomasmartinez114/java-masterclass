4121b9fba5fccf30b418f70a8a8df7e3cc92eb98
Thomas Martinez <tmartinez114@gmail.com>
2020-10-31 18:32:24 -0400

    Methods - completed

--- 04 - Expressions, Statements, Code Blocks, Methods/Methods/src/martinez/company/Main.java
+++ 04 - Expressions, Statements, Code Blocks, Methods/Methods/src/martinez/company/Main.java
@@ -8,27 +8,24 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        calculateScore(true, 800, 5, 100);
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your finale score was " + highScore);
 
         score = 1000;
         levelCompleted = 8;
         bonus = 200;
 
-        calculateScore();
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your finale score was " + highScore);
+
     }
 
     // new method
     // public static int --- will say return a value of int when it isnt set as void
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
-
         if(gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your finale score was " + finalScore);
             return finalScore;
         }
         return -1;
