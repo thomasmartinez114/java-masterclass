1ce18c06c08aad6ffc40547502ea31fa26663e90
Thomas Martinez <tmartinez114@gmail.com>
2020-10-31 18:27:58 -0400

    Methods - part 1

--- 04 - Expressions, Statements, Code Blocks, Methods/Methods/src/martinez/company/Main.java
+++ 04 - Expressions, Statements, Code Blocks, Methods/Methods/src/martinez/company/Main.java
@@ -8,19 +8,29 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        if(gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
-            System.out.println("Your finale score was " + finalScore);
-        }
+        calculateScore(true, 800, 5, 100);
 
         score = 1000;
         levelCompleted = 8;
         bonus = 200;
 
-        if (gameOver) {
+        calculateScore();
+    }
+
+    // new method
+    // public static int --- will say return a value of int when it isnt set as void
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if(gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
+            finalScore += 1000;
+            System.out.println("Your finale score was " + finalScore);
+            return finalScore;
         }
+        return -1;
     }
 }
