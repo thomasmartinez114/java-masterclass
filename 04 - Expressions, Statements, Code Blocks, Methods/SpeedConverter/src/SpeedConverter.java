2515acd352a86eb6d2bc31cf66d8fdeb7408bbbf
Thomas Martinez <tmartinez114@gmail.com>
2020-10-31 23:29:35 -0400

    Coding Exercise - Speed Converter Part 2

--- 04 - Expressions, Statements, Code Blocks, Methods/SpeedConverter/src/SpeedConverter.java
+++ 04 - Expressions, Statements, Code Blocks, Methods/SpeedConverter/src/SpeedConverter.java
@@ -8,4 +8,16 @@ public class SpeedConverter {
 
         return Math.round(kilometersPerHour / 1.609);
     }
+
+    public static void printConversion(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour +
+                    "km/h = " + milesPerHour +
+                    " mi/h");
+        }
+    }
 }
