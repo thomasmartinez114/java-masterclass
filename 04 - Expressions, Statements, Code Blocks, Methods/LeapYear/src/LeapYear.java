7081fbad4341c6b3c1a91944d0f16ffb5dfaf7e1
Thomas Martinez <tmartinez114@gmail.com>
2020-11-01 22:55:27 -0500

    LeapYear - need to finish

---
+++ 04 - Expressions, Statements, Code Blocks, Methods/LeapYear/src/LeapYear.java
@@ -0,0 +1,10 @@
+public class LeapYear {
+    public static boolean isLeapYear (int year) {
+        if (year < 1 || year > 9999) {
+            System.out.println("False");
+            return false;
+        } else if (4 % year == 0) {
+
+        }
+    }
+}
