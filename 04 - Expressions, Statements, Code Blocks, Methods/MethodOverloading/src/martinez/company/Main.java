19253c1e9f836b18299a27d0189e088154ac6d74
Thomas Martinez <tmartinez114@gmail.com>
2020-11-05 05:16:16 -0500

    Method Overloading Challenge - Solution

--- 04 - Expressions, Statements, Code Blocks, Methods/MethodOverloading/src/martinez/company/Main.java
+++ 04 - Expressions, Statements, Code Blocks, Methods/MethodOverloading/src/martinez/company/Main.java
@@ -8,8 +8,12 @@ public class Main {
         calculateScore(75);
         calculateScore();
 
-        calcFeetAndInchesToCentimeters(12, 10);
-        calcFeetAndInchesToCentimeters(2);
+        double centimeters = calcFeetAndInchesToCentimeters(6, 13);
+        if (centimeters < 0.0) {
+            System.out.println("Invalid Parameters");
+        }
+
+        calcFeetAndInchesToCentimeters(100);
     }
 
     public static int calculateScore(String playerName, int score) {
@@ -28,24 +32,76 @@ public class Main {
         return 0;
     }
 
-    public static double calcFeetAndInchesToCentimeters(int feet, int inches) {
-        if (feet > 0) {
-            double cmFeet = feet / 0.032808;
-            System.out.println("Feet to CM = " + cmFeet);
-            return cmFeet;
-        } else {
+    // Challenge
+    // Create a method called calcFeetAndInchesToCentimeters
+    // It needs to have two parameters.
+    // feet is the first parameter, inches is the 2nd parameter
+    //
+    // You should validate that the first parameter feet is >= 0
+    // You should validate that the 2nd parameter inches is >=0 and <=12
+    // return -1 from the method if either of the above is not true
+    //
+    // If the parameters are valid, then calculate how many centimetres
+    // comprise the feet and inches passed to this method and return
+    // that value.
+    //
+    // Create a 2nd method of the same name but with only one parameter
+    // inches is the parameter
+    // validate that its >=0
+    // return -1 if it is not true
+    // But if its valid, then calculate how many feet are in the inches
+    // and then here is the tricky part
+    // call the other overloaded method passing the correct feet and inches
+    // calculated so that it can calculate correctly.
+    // hints: Use double for your number datatypes is probably a good idea
+    // 1 inch = 2.54cm  and one foot = 12 inches
+    // use the link I give you to confirm your code is calculating correctly.
+    // Calling another overloaded method just requires you to use the
+    // right number of parameters.
+
+    // My submissions
+//    public static double calcFeetAndInchesToCentimeters(int feet, int inches) {
+//        if (feet > 0) {
+//            double cmFeet = feet / 0.032808;
+//            System.out.println("Feet to CM = " + cmFeet);
+//            return cmFeet;
+//        } else {
+//            return -1;
+//        }
+//    }
+//
+//    public static double calcFeetAndInchesToCentimeters(int feet) {
+//        if (feet >= 0) {
+//            double feetToInches = feet * 0.0833;
+//            System.out.println(feetToInches);
+//            return feetToInches;
+//        } else {
+//            return -1;
+//        }
+//    }
+
+    // Answer
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        if((feet < 0) || ((inches < 0) || (inches > 12))) {
+            System.out.println("Invalid feet or inches parameters");
             return -1;
         }
+
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet, " + inches + " inches = " + centimeters + " cm");
+        return centimeters;
     }
 
-    public static double calcFeetAndInchesToCentimeters(int feet) {
-        if (feet >= 0) {
-            double feetToInches = feet * 0.0833;
-            System.out.println(feetToInches);
-            return feetToInches;
-        } else {
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+        if (inches < 0) {
             return -1;
         }
+
+        double feet = (int) inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " inches is equal to " + feet + " feet and " + remainingInches + " inches");
+        return calcFeetAndInchesToCentimeters(feet, remainingInches);
     }
 
 }
