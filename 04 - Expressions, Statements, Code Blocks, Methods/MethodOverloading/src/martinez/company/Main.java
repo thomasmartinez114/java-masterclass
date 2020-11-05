ee071083d4e5f27eb57e0ae83c9094d7a1497036
Thomas Martinez <tmartinez114@gmail.com>
2020-11-05 01:05:46 -0500

    Method Overloading example

--- 04 - Expressions, Statements, Code Blocks, Methods/MethodOverloading/src/martinez/company/Main.java
+++ 04 - Expressions, Statements, Code Blocks, Methods/MethodOverloading/src/martinez/company/Main.java
@@ -5,10 +5,23 @@ public class Main {
     public static void main(String[] args) {
         int newScore = calculateScore("Tom", 500);
         System.out.println("New score is " + newScore);
+        calculateScore(75);
+        calculateScore();
     }
 
     public static int calculateScore(String playerName, int score) {
         System.out.println("Player " + playerName + " scored " + score + " points");
         return score * 1000;
+    }
+
+    // Method Overloading
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player " + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name, no player score");
+        return 0;
     }
 }
