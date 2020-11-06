3f12a0a4feaa5782357dfacc661cb0b90ac44cef
Thomas Martinez <tmartinez114@gmail.com>
2020-11-06 18:32:47 -0500

    SecondsANdMinutes Challenge - SOLUTION

--- 04 - Expressions, Statements, Code Blocks, Methods/SecondsAndMinutesChallenge/src/martinez/company/Main.java
+++ 04 - Expressions, Statements, Code Blocks, Methods/SecondsAndMinutesChallenge/src/martinez/company/Main.java
@@ -2,10 +2,6 @@ package martinez.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-        getDurationString(60, 59);
-    }
-
     // Challenge
 
     // Create method getDurationString(int minutes, int seconds)
@@ -16,32 +12,12 @@ public class Main {
     // the minutes and seconds passed to this method and return
     // that value as string in format "XXh YYm ZZs"
 
-    public static int getDurationString(int minutes, int seconds){
-        if ((minutes < 0) || (seconds < 0) || (seconds > 59)) {
-            System.out.println("Invalid Value");
-            return -1;
-        }
-
-        int hours = minutes / 60;
-        System.out.println(hours + "h " + minutes + "m " + seconds + "s");
-        return hours;
-    }
-
     // Create 2nd method with same name with only one param seconds
     // Validate that it is >= 0 and return "Invalid value" if not true
     // If valid, calc how many minutes are in the seconds value then
     // call the other overloaded method passing the correct minutes and seconds
     // calculated so that it can calculate correctly
     // Call both methods to print values to console
-
-    public static int getDurationString(int seconds) {
-        if (seconds < 0) {
-            System.out.println("Invalid Value");
-        }
-
-        int minutes = 60 / seconds;
-        System.out.println("Minutes: " + minutes);
-    }
 
     // Tips
     // Use int or long for number data types
@@ -50,4 +26,56 @@ public class Main {
     // Bonus
     // For the input 61 mins output should be 01h 01m 00s, but ok if it is
     // 1h 1m 0s (use if-else)
+
+
+    public static void main(String[] args) {
+        System.out.println(getDurationString(65L, 45L));
+        System.out.println(getDurationString(3945L));
+    }
+
+    //    public static int getDurationString(int minutes, int seconds){
+//        if ((minutes < 0) || (seconds < 0) || (seconds > 59)) {
+//            System.out.println("Invalid Value");
+//            return -1;
+//        }
+//
+//        int hours = minutes / 60;
+//        System.out.println(hours + "h " + minutes + "m " + seconds + "s");
+//        return hours;
+//    }
+
+    //    public static int getDurationString(int seconds) {
+//        if (seconds < 0) {
+//            System.out.println("Invalid Value");
+//        }
+//
+//        int minutes = 60 / seconds;
+//        System.out.println("Minutes: " + minutes);
+//    }
+
+    //////////////////////////////////
+    //            Solution         //
+    /////////////////////////////////
+
+    private static String getDurationString(long minutes, long seconds) {
+        if( (minutes < 0) || (seconds < 0) || (seconds > 59) ) {
+            return "Invalid Value";
+        }
+
+        long hours = minutes / 60;
+        long remainingMinutes = minutes % 60;
+        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+    }
+
+    private static String getDurationString(long seconds) {
+        if (seconds < 0) {
+            return "Invalid Value";
+        }
+
+        long minutes = seconds / 60;
+        long remainingSeconds = seconds % 60;
+
+        return getDurationString(minutes, remainingSeconds);
+    }
+
 }
