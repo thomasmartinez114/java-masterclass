0b4b8c6fcd87704243ef5949cd44e27309fb7bb8
Thomas Martinez <tmartinez114@gmail.com>
2020-11-07 09:34:49 -0500

    SecondsANdMinutes Challenge - Solution complete

--- 04 - Expressions, Statements, Code Blocks, Methods/SecondsAndMinutesChallenge/src/martinez/company/Main.java
+++ 04 - Expressions, Statements, Code Blocks, Methods/SecondsAndMinutesChallenge/src/martinez/company/Main.java
@@ -32,6 +32,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(getDurationString(65L, 45L));
         System.out.println(getDurationString(3945L));
+        System.out.println(getDurationString(-41));
+        System.out.println(getDurationString(65, 9));
     }
 
     //    public static int getDurationString(int minutes, int seconds){
