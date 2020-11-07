e4c2d9170a521990e2f1b4570c827e2dc0f2b866
Thomas Martinez <tmartinez114@gmail.com>
2020-11-07 10:41:00 -0500

    MinsToYear - Printed out the years and days need to use modulus

--- 04 - Expressions, Statements, Code Blocks, Methods/MinsToYearsAndDays/src/MinsToYears.java
+++ 04 - Expressions, Statements, Code Blocks, Methods/MinsToYearsAndDays/src/MinsToYears.java
@@ -7,6 +7,14 @@ public class MinsToYears {
             System.out.println(INVALID_VALUE);
         }
 
+        long hours = minutes / 60;
+        //System.out.println("HOURS " + hours);
+        long days = hours / 24;
+        //System.out.println("DAYS " + days);
+        long years = days / 365;
+        //System.out.println("YEARS " + years);
+
         // print XX min = YY y and ZZ d
+        System.out.println(minutes + " min = " + years + " y and " + days + " d");
     }
 }
