bf3320ad3d7d8b107f0566fbdee014195313df81
Thomas Martinez <tmartinez114@gmail.com>
2020-11-07 10:19:30 -0500

    AreaCalculator - solved

--- 04 - Expressions, Statements, Code Blocks, Methods/AreaCalculator/src/AreaCalculator.java
+++ 04 - Expressions, Statements, Code Blocks, Methods/AreaCalculator/src/AreaCalculator.java
@@ -1,11 +1,14 @@
 public class AreaCalculator {
 
     public static final double INVALID_VALUE = -1.0;
+    public static final double PI = Math.PI;
 
     public static double area(double radius) {
         if (radius < 0) {
             return INVALID_VALUE;
         }
+        double circle = radius * radius * PI;
+        return circle;
     }
 
     public static double area(double x, double y) {
@@ -13,5 +16,7 @@ public class AreaCalculator {
             return INVALID_VALUE;
         }
 
+        double triangleSum = x * y;
+        return triangleSum;
     }
 }
--- 04 - Expressions, Statements, Code Blocks, Methods/AreaCalculator/src/Main.java
+++ 04 - Expressions, Statements, Code Blocks, Methods/AreaCalculator/src/Main.java
@@ -3,9 +3,9 @@ import java.awt.geom.Area;
 public class Main {
 
     public static void main(String[] args) {
-        AreaCalculator.area(5.0); // return 78.53975
-        AreaCalculator.area(-1); // return -1
-        AreaCalculator.area(5.0, 4.0); // return 20.0
-        AreaCalculator.area(-1.0, 4.0); // return -1
+        System.out.println(AreaCalculator.area(5.0)); // return 78.53975
+        System.out.println(AreaCalculator.area(-1)); // return -1
+        System.out.println(AreaCalculator.area(5.0, 4.0)); // return 20.0
+        System.out.println(AreaCalculator.area(-1.0, 4.0)); // return -1
     }
 }
