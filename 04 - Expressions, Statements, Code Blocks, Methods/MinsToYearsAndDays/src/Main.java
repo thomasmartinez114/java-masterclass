4ca71f7107917a6db9022963618eb9c08940883e
Thomas Martinez <tmartinez114@gmail.com>
2020-11-07 10:29:48 -0500

    MinsToYear - Started

---
+++ 04 - Expressions, Statements, Code Blocks, Methods/MinsToYearsAndDays/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        MinsToYears.printYearsAndDays(525600); // 525600 min = 1 y and 0 d
+        MinsToYears.printYearsAndDays(1051200); // 1051200 min = 2 y and 0 d
+        MinsToYears.printYearsAndDays(561600); // 561600 min = 1 y and 25 d
+    }
+}
---
+++ 04 - Expressions, Statements, Code Blocks, Methods/MinsToYearsAndDays/src/MinsToYears.java
@@ -0,0 +1,12 @@
+public class MinsToYears {
+
+    public static final String INVALID_VALUE = "Invalid Value";
+
+    public static void printYearsAndDays(long minutes){
+        if (minutes < 0){
+            System.out.println(INVALID_VALUE);
+        }
+
+        // print XX min = YY y and ZZ d
+    }
+}
