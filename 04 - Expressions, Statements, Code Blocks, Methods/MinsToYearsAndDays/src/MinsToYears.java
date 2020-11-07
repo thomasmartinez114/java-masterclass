930ae5a8ff3f48341bc5c3ecf7c8d628407c99c6
Thomas Martinez <tmartinez114@gmail.com>
2020-11-07 11:07:11 -0500

    MinsToYear - Completed

--- 04 - Expressions, Statements, Code Blocks, Methods/MinsToYearsAndDays/src/MinsToYears.java
+++ 04 - Expressions, Statements, Code Blocks, Methods/MinsToYearsAndDays/src/MinsToYears.java
@@ -5,16 +5,17 @@ public class MinsToYears {
     public static void printYearsAndDays(long minutes){
         if (minutes < 0){
             System.out.println(INVALID_VALUE);
+        } else {
+            long hours = minutes / 60;
+            //System.out.println("HOURS " + hours);
+            long days = hours / 24;
+            long remainingDays = days % 365;
+            //System.out.println("DAYS " + days);
+            long years = days / 365;
+            //System.out.println("YEARS " + years);
+
+            // print XX min = YY y and ZZ d
+            System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
         }
-
-        long hours = minutes / 60;
-        //System.out.println("HOURS " + hours);
-        long days = hours / 24;
-        //System.out.println("DAYS " + days);
-        long years = days / 365;
-        //System.out.println("YEARS " + years);
-
-        // print XX min = YY y and ZZ d
-        System.out.println(minutes + " min = " + years + " y and " + days + " d");
     }
 }
