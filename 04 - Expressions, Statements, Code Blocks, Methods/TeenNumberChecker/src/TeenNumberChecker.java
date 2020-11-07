8ecb6254c3a286c8c5f212cad02e8e195c4b920f
Thomas Martinez <tmartinez114@gmail.com>
2020-11-07 14:52:17 -0500

    Teen CHecker - SOLVED

---
+++ 04 - Expressions, Statements, Code Blocks, Methods/TeenNumberChecker/src/TeenNumberChecker.java
@@ -0,0 +1,18 @@
+public class TeenNumberChecker {
+
+    public static boolean hasTeen(int age1, int age2, int age3){
+        if ( age1 >= 13 && age1 <= 19 || age2 >= 13 && age2 <= 19 || age3 >= 13 && age3 <= 19){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isTeen(int age){
+        if (age >= 13 && age <= 19){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
