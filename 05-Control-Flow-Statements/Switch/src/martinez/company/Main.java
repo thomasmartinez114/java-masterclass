fa79bc8bf6875fe55f1de4fe46ce3d1db7474efe
Thomas Martinez <tmartinez114@gmail.com>
2020-11-11 20:03:25 -0500

    Switch - Challenge - My solution

--- 05-Control-Flow-Statements/Switch/src/martinez/company/Main.java
+++ 05-Control-Flow-Statements/Switch/src/martinez/company/Main.java
@@ -30,6 +30,22 @@ public class Main {
                 break;
         }
 
-        // More code here
+        // Create a new switch statement using char instead of int
+        // create a new char variable
+        // create a switch statement testing for
+        // A, B, C, D, or E
+        // display a message if any of these are found and then break
+        // Add a default which displays a message saying not found
+
+        String charValue = "F";
+
+        switch(charValue) {
+            case "A": case "B": case "C": case "D": case "E":
+                System.out.println("The char was A, B, C, D or E: " + charValue);
+                break;
+
+            default:
+                System.out.println("The char is not A, B, C, D or E: " + charValue);
+        }
     }
 }
