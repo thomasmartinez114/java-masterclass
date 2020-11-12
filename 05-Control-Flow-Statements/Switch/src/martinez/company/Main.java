4a98fb89e9b66da55cdec66cd6aea06f0b99292a
Thomas Martinez <tmartinez114@gmail.com>
2020-11-11 19:27:44 -0500

    Switch Value - using chained cases

--- 05-Control-Flow-Statements/Switch/src/martinez/company/Main.java
+++ 05-Control-Flow-Statements/Switch/src/martinez/company/Main.java
@@ -21,6 +21,10 @@ public class Main {
             case 2:
                 System.out.println("Value was 2");
                 break;
+            case 3: case 4: case 5:
+                System.out.println("Was 3, or a 4, or 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
             default:
                 System.out.println("Was not 1 or 2");
                 break;
