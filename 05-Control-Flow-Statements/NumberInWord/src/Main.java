58f312b03edac71d49327a494cd2b96d11c1183a
Thomas Martinez <tmartinez114@gmail.com>
2020-11-11 20:39:00 -0500

    NumberInWord - SOLVED

---
+++ 05-Control-Flow-Statements/NumberInWord/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        NumberInWord.printNumberInWord(0);
+        NumberInWord.printNumberInWord(10);
+        NumberInWord.printNumberInWord(-1);
+    }
+}
