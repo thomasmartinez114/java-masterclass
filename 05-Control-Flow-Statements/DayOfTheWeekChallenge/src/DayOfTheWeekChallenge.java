2a3011659c41b336f67e13b30aec45c6062c24a8
Thomas Martinez <tmartinez114@gmail.com>
2020-11-11 20:24:50 -0500

    DayOfWeekChallenge - Corrections - added breaks, defaults and else if

--- 05-Control-Flow-Statements/DayOfTheWeekChallenge/src/DayOfTheWeekChallenge.java
+++ 05-Control-Flow-Statements/DayOfTheWeekChallenge/src/DayOfTheWeekChallenge.java
@@ -15,55 +15,53 @@ public class DayOfTheWeekChallenge {
     public static void printDayOfTheWeek(int day) {
 
         switch(day){
-            case 0: {
+            case 0:
                 System.out.println("Sunday");
                 break;
-            }
-            case 1: {
+            case 1:
                 System.out.println("Monday");
                 break;
-            }
-            case 2: {
+            case 2:
                 System.out.println("Tuesday");
                 break;
-            }
-            case 3: {
+            case 3:
                 System.out.println("Wednesday");
                 break;
-            }
-            case 4: {
+            case 4:
                 System.out.println("Thursday");
                 break;
-            }
-            case 5: {
+            case 5:
                 System.out.println("Friday");
                 break;
-            }
-            case 6: {
+            case 6:
                 System.out.println("Saturday");
                 break;
-            }
+            default:
+                System.out.println("Invalid Day");
+                break;
         }
 
         if (day == 0){
             System.out.println("Day is Sunday");
-        } if (day == 1) {
+        } else if (day == 1) {
             System.out.println("Day is Mon");
         }
-        if (day == 2) {
+        else if (day == 2) {
             System.out.println("Day is Tues");
         }
-        if (day == 3) {
+        else if (day == 3) {
             System.out.println("Day is Wed");
         }
-        if (day == 4) {
+        else if (day == 4) {
             System.out.println("Day is Thurs");
         }
-        if (day == 5) {
+        else if (day == 5) {
             System.out.println("Day is Fri");
         }
-        if (day == 6) {
+        else if (day == 6) {
             System.out.println("Day is Sat");
+        } else {
+            System.out.println("Invalid Day");
         }
     }
 }
--- 05-Control-Flow-Statements/DayOfTheWeekChallenge/src/Main.java
+++ 05-Control-Flow-Statements/DayOfTheWeekChallenge/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         DayOfTheWeekChallenge.printDayOfTheWeek(2);
-        DayOfTheWeekChallenge.printDayOfTheWeek(0);
+        DayOfTheWeekChallenge.printDayOfTheWeek(-1);
         DayOfTheWeekChallenge.printDayOfTheWeek(4);
     }
 }
