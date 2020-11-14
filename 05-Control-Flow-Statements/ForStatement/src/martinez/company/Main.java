c9e41fecdd018c30c61f0a0d5dbe94a15bb8e27a
Thomas Martinez <tmartinez114@gmail.com>
2020-11-14 14:23:13 -0500

    For Loop Challenge  - have it count down to 2 percent

--- 05-Control-Flow-Statements/ForStatement/src/martinez/company/Main.java
+++ 05-Control-Flow-Statements/ForStatement/src/martinez/company/Main.java
@@ -13,7 +13,14 @@ public class Main {
         // the amount of 10000 with an interestRate of 2,3,4,5,6,7, and 8
         // and print the results to the console window.
 
-        for (int i = 2; i < 9; i++) {
+//        for (int i = 2; i < 9; i++) {
+//            System.out.println("$10,000 at " + i + "% interest rate = $"
+//                    + String.format("%.2f",calculateInterest(10000.0, i)));
+//        }
+
+        // How would you modify the for loop above to do the same thing as
+        // shown but to start from 8% and work back to 2%
+        for (int i = 8; i > 1; i--) {
             System.out.println("$10,000 at " + i + "% interest rate = $"
                     + String.format("%.2f",calculateInterest(10000.0, i)));
         }
