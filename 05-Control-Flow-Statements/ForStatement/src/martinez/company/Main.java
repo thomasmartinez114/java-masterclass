1658845faae7365830919f1d257380cf76b79645
Thomas Martinez <tmartinez114@gmail.com>
2020-11-14 14:40:23 -0500

    For Loop - prime numbers

--- 05-Control-Flow-Statements/ForStatement/src/martinez/company/Main.java
+++ 05-Control-Flow-Statements/ForStatement/src/martinez/company/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
         for (int i = 2; i < 9; i++) {
             System.out.println("$10,000 at " + i + "% interest rate = $"
-                    + String.format("%.2f",calculateInterest(10000.0, i)));
+                    + String.format("%.2f", calculateInterest(10000.0, i)));
         }
 
         System.out.println("***********************");
@@ -26,10 +26,44 @@ public class Main {
         // shown but to start from 8% and work back to 2%
         for (int i = 8; i > 1; i--) {
             System.out.println("$10,000 at " + i + "% interest rate = $"
-                    + String.format("%.2f",calculateInterest(10000.0, i)));
+                    + String.format("%.2f", calculateInterest(10000.0, i)));
         }
 
+        // Create a for statement using any range of numbers
+        // Determine if the number is a prime number using the isPrime method
+        // if it is a prime number, print it out AND increment a count of the
+        // number of prime numbers found
+        // if that count is 3 exit the for loop
+        // hint:  Use the break; statement to exit
+
+        int count = 0;
+        for (int i = 10; i < 50; i++){
+            if (isPrime(i)) {
+                count++;
+                System.out.println("Number " + i + " is a Prime Number");
+                if(count == 3) {
+                    System.out.println("Exiting for loop");
+                    break;
+                }
+            }
+        }
+
+
+
     }
+
+        public static boolean isPrime(int n) {
+            if(n == 1) {
+                return false;
+            }
+
+            for(int i = 2; i <= n/2; i++) {
+                if(n % 1 == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
 
     public static double calculateInterest(double amount, double interestRate){
         return(amount * (interestRate / 100));
