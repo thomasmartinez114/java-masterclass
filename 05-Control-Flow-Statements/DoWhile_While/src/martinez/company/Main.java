de62ced2b58bc0eb0455fd72182a94936dea5980
Thomas Martinez <tmartinez114@gmail.com>
2020-12-26 13:53:19 -0500

    Lecture - isEvenNumber - my Submission

--- 05-Control-Flow-Statements/DoWhile_While/src/martinez/company/Main.java
+++ 05-Control-Flow-Statements/DoWhile_While/src/martinez/company/Main.java
@@ -24,4 +24,15 @@ public class Main {
             count++;
         } while (count != 6);
     }
+
+    // Create a method called isEvenNumber that takes a parameter of type int
+    // Its purpose is to determine if the argument passed to the method is
+    // an even number or not.
+    // return true if an even number, otherwise return false;
+
+    public static boolean isEvenNumber(int number) {
+        do {
+            return true;
+        } while (number % 2 == 0);
+    } return false;
 }
