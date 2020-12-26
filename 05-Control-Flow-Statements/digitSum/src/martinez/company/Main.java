c94c4141aecf11a5b2ffc35ee5210ecdf89e1d50
Thomas Martinez <tmartinez114@gmail.com>
2020-12-26 17:38:51 -0500

    sumDigits 2 done

--- 05-Control-Flow-Statements/digitSum/src/martinez/company/Main.java
+++ 05-Control-Flow-Statements/digitSum/src/martinez/company/Main.java
@@ -28,6 +28,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("The sum of the digits in 125 is " + sumDigits(125));
+        System.out.println("The sum of the digits in -125 is " + sumDigits(-125));
+        System.out.println("The sum of the digits in 4 is " + sumDigits(4));
+        System.out.println("The sum of the digits in 32123 is " + sumDigits(32123));
+
     }
 
     public static int sumDigits(int number) {
