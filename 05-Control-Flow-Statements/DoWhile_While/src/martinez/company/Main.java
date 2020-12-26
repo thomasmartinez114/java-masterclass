b2531374d30341e3719819417ac5c50a258764a1
Thomas Martinez <tmartinez114@gmail.com>
2020-12-26 13:23:16 -0500

    While doesnt run since count = 6

--- 05-Control-Flow-Statements/DoWhile_While/src/martinez/company/Main.java
+++ 05-Control-Flow-Statements/DoWhile_While/src/martinez/company/Main.java
@@ -3,19 +3,19 @@ package martinez.company;
 public class Main {
 
     public static void main(String[] args) {
-        int count = 0;
-        while(count != 5) {
+        int count = 6;
+        while(count != 6) {
             System.out.println("Count value is " + count);
             count++;
         }
 
-        count = 1;
-        while(true) {
-            if(count == 6) {
-                break;
-            }
-            System.out.println("Count value is " + count);
-            count++;
-        }
+//        count = 1;
+//        while(true) {
+//            if(count == 6) {
+//                break;
+//            }
+//            System.out.println("Count value is " + count);
+//            count++;
+//        }
     }
 }
