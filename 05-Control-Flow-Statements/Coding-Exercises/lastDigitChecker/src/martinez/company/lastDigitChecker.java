7b4d24d73b3c4945e44e0e584925355d44851e67
Thomas Martinez <tmartinez114@gmail.com>
2020-12-27 15:55:10 -0500

    Parsing Values from a String --- Integer.parseInt()

---
+++ 05-Control-Flow-Statements/Coding-Exercises/lastDigitChecker/src/martinez/company/lastDigitChecker.java
@@ -0,0 +1,10 @@
+package martinez.company;
+
+public class lastDigitChecker {
+
+    public static boolean hasSameLastDigit(int num1, int num2, int num3) {
+
+
+        if (num1 < 10 || num1 > 1000)
+    }
+}
---
+++ 05-Control-Flow-Statements/parsing_values_from_string/src/martinez/company/Main.java
@@ -0,0 +1,20 @@
+package martinez.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String numberAsString = "2018";
+        System.out.println("numberAsString= " + numberAsString);
+
+        int number = Integer.parseInt(numberAsString);
+        System.out.println("number = " + number);
+
+        // test the parsing
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString = " + numberAsString);
+        System.out.println("number = " + number);
+    }
+}
