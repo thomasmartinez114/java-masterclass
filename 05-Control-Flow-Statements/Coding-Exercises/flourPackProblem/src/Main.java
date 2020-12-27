68e66f927b6721b6c79e25c42438d2be0f6859ff
Thomas Martinez <tmartinez114@gmail.com>
2020-12-27 17:16:25 -0500

    flourPackProblem solved

---
+++ 05-Control-Flow-Statements/Coding-Exercises/flourPackProblem/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main (String[] args) {
+        System.out.println(flourPackProblem.canPack(1, 0, 5));
+        System.out.println(flourPackProblem.canPack(2, 2, 11));
+    }
+}
