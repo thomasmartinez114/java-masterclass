6a9e1d95a20cb6e48f315b7cc40937094793527d
Thomas Martinez <tmartinez114@gmail.com>
2020-12-27 16:01:23 -0500

    Double parsing

--- 05-Control-Flow-Statements/parsing_values_from_string/src/martinez/company/Main.java
+++ 05-Control-Flow-Statements/parsing_values_from_string/src/martinez/company/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String numberAsString = "2018";
+        String numberAsString = "2018.125";
         System.out.println("numberAsString= " + numberAsString);
 
-        int number = Integer.parseInt(numberAsString);
+        double number = Double.parseDouble(numberAsString);
         System.out.println("number = " + number);
 
         // test the parsing
