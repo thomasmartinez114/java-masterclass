d94abaf227b1b8a036f9f4b0723e9dd35e5183da
Thomas Martinez <tmartinez114@gmail.com>
2021-01-09 14:55:23 -0500

    user input is skipping over the name input ** must fix

--- 05-Control-Flow-Statements/ReadingUserInput/src/Main.java
+++ 05-Control-Flow-Statements/ReadingUserInput/src/Main.java
@@ -4,10 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your year of birth: ");
+        int yearOfBirth = scanner.nextInt();
+
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
+        int age = 2020 - yearOfBirth;
 
-        System.out.println("Your name is " + name);
+        System.out.println("Your name is " + name + " and age is " + age);
 
         scanner.close();
     }
