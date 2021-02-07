cebcea1b7e2996aaaea449f839f4c41e33f1e11b
Thomas Martinez <tmartinez114@gmail.com>
2021-02-07 17:11:42 -0500

    logic is functiononig

--- 05-Control-Flow-Statements/Coding-Exercises/ReadingUserInputChallenge/MinAndMaxInputChallenge/src/Main.java
+++ 05-Control-Flow-Statements/Coding-Exercises/ReadingUserInputChallenge/MinAndMaxInputChallenge/src/Main.java
@@ -6,8 +6,34 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        
+        int min = 0;
+        int max = 0;
 
+        while(true) {
+            System.out.println("Enter number: ");
+            boolean isAnInt = scanner.hasNextInt();
+
+            if(isAnInt) {
+
+                int number = scanner.nextInt();
+
+                // test if number is > max
+                if(number > max) {
+                    max = number;
+                }
+
+                if(number < min) {
+                    min = number;
+                }
+
+            } else {
+                break;
+            }
+
+            scanner.nextLine(); // handle input
+        }
+
+        System.out.println("Min= " + min + ", max= " + max);
         scanner.close();
     }
 }
